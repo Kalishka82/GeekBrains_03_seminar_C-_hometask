ad3aa29a8498bb9ace99b438a53adf58493c9f28
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-08 15:04:22 +0300

    add hometask 23 Cubes Of Number

---
+++ ex_23/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// Задача 23: Напишите программу, которая принимает на вход число 
+// (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Input number: ");
+int N = int.Parse(Console.ReadLine()!);
+
+    for (int i = 1; i <= N; i++)
+    {
+        Console.Write(Math.Pow(i, 3));
+        if (i < N)
+        Console.Write(", ");
+    }
